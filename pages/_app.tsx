78845bb7db7f284c24ee9cad818a80ae7aac0071
raja <raja.rakoto7@gmail.com>
2023-03-08 05:26:25 +0300

    apply sseo basics for globals styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from '@mui/material/styles';
 import type { AppProps } from 'next/app';
-import '../styles/globals.css';
+import '../styles/globals.min.css';
 import theme from '../theme';
 import { NextPageWithLayout } from './page';
 
