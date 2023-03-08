e43a133853b0efe5a98aacd4993edcdf253d6dc8
raja <raja.rakoto7@gmail.com>
2023-03-08 14:56:33 +0300

    apply new style for card component

--- components/participant/card.tsx
+++ components/participant/card.tsx
@@ -1,25 +1,14 @@
-import VisibilityIcon from '@mui/icons-material/Visibility';
-import {
-  Avatar,
-  AvatarGroup,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Paper,
-  Typography,
-} from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import { VotesParticipant } from '../type';
+import { Avatar, AvatarGroup, CardContent, CardMedia } from '@mui/material'
+import { VotesParticipant } from '../type'
+import styles from './card.module.css'
 
 type params = {
-  name: string;
-  logoSrc: string;
-  votes: string;
-  voters: VotesParticipant[] | null;
-  againstVoters: VotesParticipant[] | null;
-};
+  name: string
+  logoSrc: string
+  votes: string
+  voters: VotesParticipant[] | null
+  againstVoters: VotesParticipant[] | null
+}
 
 export default function ParticipantCard({
   name,
@@ -28,27 +17,27 @@ export default function ParticipantCard({
   voters,
   againstVoters,
 }: params) {
-  const classes = useStyles();
+  // const classes = useStyles();
 
   return (
-    <Paper elevation={4} className={classes.paper}>
-      <Card className={classes.card}>
-        <CardMedia
-          component="img"
-          height="120px"
-          image={process.env.API_IMG + logoSrc}
-          className={classes.cardMedia}
-        />
-        <CardContent>
+    <div className={styles.card}>
+      <CardMedia
+        component="img"
+        height="120px"
+        image={process.env.API_IMG + logoSrc}
+        className={styles.cardMedia}
+      />
 
-          <h3>
-            {name}
-          </h3>
-
-          <div className={classes.votes}>
-            <Typography variant="body1" color="text.secondary">
-              {votes}% alainay
-            </Typography>
+      <CardContent>
+        <hr />
+        <h3 className={styles.title}>{name}</h3>
+        <hr />
+        <div className={styles.alainay}>
+          <div>
+            <h2>{votes}%</h2>
+            <span>Alainay</span>
+          </div>
+          <div>
             <AvatarGroup max={4}>
               {voters &&
                 voters.map((element, index) => (
@@ -60,62 +49,28 @@ export default function ParticipantCard({
                 ))}
             </AvatarGroup>
           </div>
-
-          <div className={classes.votes}>
-            <Typography variant="body2" color="error">
-              Zakanay
-            </Typography>
-            <AvatarGroup max={4}>
-              {againstVoters &&
-                againstVoters.map((element, index) => (
-                  <Avatar
-                    key={index}
-                    src={element.expand.voter.profil_pic}
-                    alt={element.expand.voter.name}
-                  />
-                ))}
-            </AvatarGroup>
+        </div>
+        <div className={styles.zakanay}>
+          <AvatarGroup max={4}>
+            {againstVoters &&
+              againstVoters.map((element, index) => (
+                <Avatar
+                  key={index}
+                  src={element.expand.voter.profil_pic}
+                  alt={element.expand.voter.name}
+                />
+              ))}
+          </AvatarGroup>
+          <div>
+            <h2>0 %</h2>
+            <span>Zakanay</span>
           </div>
-        </CardContent>
-        <CardActions>
-          <Button
-            variant="outlined"
-            size="medium"
-            startIcon={<VisibilityIcon />}
-          >
-            Détails
-          </Button>
-        </CardActions>
-      </Card>
-    </Paper>
-  );
-}
+        </div>
+      </CardContent>
 
-const useStyles = makeStyles(() => ({
-  paper: {
-    marginTop: 45,
-  },
-  votes: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  card: {
-    width: '280px',
-    height: '320px',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-  },
-  cardMedia: {
-    objectFit: 'scale-down',
-    width: '300',
-  },
-  name: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontFamily:
-      'Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,Bitstream Vera Sans Mono, Courier New, monospace',
-  },
-}));
+      <button className={styles.secondary_btn} role="button">
+        Details
+      </button>
+    </div>
+  )
+}
