e42dd28348bcf7eb6bb8ef4f36924ac449311743
raja <raja.rakoto7@gmail.com>
2023-03-08 08:48:43 +0300

    set SidebarLayout global style

--- components/layouts/sidebar/SidebarLayout.tsx
+++ components/layouts/sidebar/SidebarLayout.tsx
@@ -11,19 +11,22 @@ const SidebarLayout: React.FC<ISidebarLayout> = () => {
     <AppBar position="fixed" color="secondary" className={styles.navbar}>
       <Toolbar>
         <Image
+          className={styles.logo}
           src="https://iteam-s.mg/assets/img/LOGO.png"
           alt="logo"
           width={50}
           height={50}
-          style={{ marginRight: '10px', width: '100', height: '100' }}
+          style={{ marginRight: '1.5rem', width: '100', height: '100' }}
         />
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography className={styles.title} variant="h5" component="div" sx={{ flexGrow: 1 }}>
           Web Ranking
         </Typography>
         <Stack spacing={2} direction="row">
           <nav className={styles.nav}>
             <Link href="/contact" legacyBehavior>
+            <button className={styles.primary_btn} role="button">
               <a>Voters</a>
+            </button>
             </Link>
           </nav>
         </Stack>
