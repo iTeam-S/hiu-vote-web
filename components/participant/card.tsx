76ba26a07a0dc371fa7f92751e3acb671f10112d
Raja Rakotonirina <raja.rakoto7@gmail.com>
2023-03-09 19:32:55 +0000

    Design

--- components/participant/card.tsx
+++ components/participant/card.tsx
@@ -1,4 +1,4 @@
-import { Avatar, AvatarGroup, CardContent, CardMedia } from '@mui/material'
+import { Avatar, AvatarGroup, CardContent } from '@mui/material'
 import { VotesParticipant } from '../type'
 import styles from './card.module.css'
 import { AiFillHeart } from 'react-icons/ai'
@@ -7,21 +7,21 @@ import { GiStrong } from 'react-icons/gi'
 import { useState } from 'react'
 
 type params = {
-  id: string,
+  id: string
   name: string
   logoSrc: string
   votes: string
   voters: VotesParticipant[] | null
-  againstVoters: VotesParticipant[] | null,
-  handleClickDetails: (id: string) => void;
+  againstVoters: VotesParticipant[] | null
+  handleClickDetails: (id: string) => void
 }
 
 type paramsButton = {
-  id: string,
-  handleClickDetails: (id: string) => void;
+  id: string
+  handleClickDetails: (id: string) => void
 }
 
-const DetailsButton = ({id, handleClickDetails}: paramsButton) => {
+const DetailsButton = ({ id, handleClickDetails }: paramsButton) => {
   const [onHover, setOnHover] = useState(false)
 
   const changerEtatSurvole = () => {
@@ -48,20 +48,14 @@ export default function ParticipantCard({
   votes,
   voters,
   againstVoters,
-  handleClickDetails
+  handleClickDetails,
 }: params) {
-  // const classes = useStyles();
-
   return (
     <div className={styles.card}>
-      <CardMedia
-        component="img"
-        height="120px"
-        image={process.env.API_URL + 'files/'+logoSrc}
-        className={styles.cardMedia}
-      />
-
-      <CardContent>
+      <div className={styles.logo}>
+        <img src={process.env.API_URL + 'files/' + logoSrc} alt="logo" />
+      </div>
+      <div className={styles.content}>
         <hr />
         <h3 className={styles.title}>{name}</h3>
         <hr />
@@ -73,36 +67,51 @@ export default function ParticipantCard({
             </span>
           </div>
           <div>
-            <AvatarGroup max={4}>
-              {voters &&
-                voters.map((element, index) => (
-                  <Avatar
-                    key={index}
-                    src={element.expand.voter.profil_pic}
-                    alt={element.expand.voter.name}
-                  />
-                ))}
-            </AvatarGroup>
+            <div style={{ width: 75 }}>
+              <AvatarGroup
+                max={4}
+                sx={{
+                  '& .MuiAvatar-root': { width: 24, height: 24, fontSize: 15 },
+                }}
+              >
+                {voters &&
+                  voters.map((element, index) => (
+                    <Avatar
+                      key={index}
+                      src={element.expand.voter.profil_pic}
+                      alt={element.expand.voter.name}
+                    />
+                  ))}
+              </AvatarGroup>
+            </div>
           </div>
         </div>
         <div className={styles.zakanay}>
-          <AvatarGroup max={3}>
-            {againstVoters &&
-              againstVoters.map((element, index) => (
-                <Avatar
-                  key={index}
-                  src={element.expand.voter.profil_pic}
-                  alt={element.expand.voter.name}
-                />
-              ))}
-          </AvatarGroup>
-
+          <div>
+            <h2>0</h2>
             <span>
               <GiStrong size={25} /> &nbsp; Zakanay
             </span>
+          </div>
+          <div>
+            <div style={{ width: 75 }}>
+              <AvatarGroup max={3}>
+                {againstVoters &&
+                  againstVoters.map((element, index) => (
+                    <Avatar
+                      key={index}
+                      src={element.expand.voter.profil_pic}
+                      alt={element.expand.voter.name}
+                    />
+                  ))}
+              </AvatarGroup>
+            </div>
+          </div>
         </div>
-      </CardContent>
-      <DetailsButton handleClickDetails={handleClickDetails} id={id}/>
+      </div>
+      <div className={styles.footer}>
+        <DetailsButton handleClickDetails={handleClickDetails} id={id} />
+      </div>
     </div>
   )
 }
--- components/voters/index.tsx
+++ components/voters/index.tsx
@@ -1,55 +1,62 @@
-import { Grid, Box, Typography } from '@mui/material';
-import { styled } from '@mui/system';
-import { ItemVoter } from '../type';
-import { useRef } from 'react';
+import { Grid, Box, Typography } from '@mui/material'
+import { styled } from '@mui/system'
+import { ItemVoter } from '../type'
+import { useRef } from 'react'
 
 const UserCard = styled(Box)({
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
-});
+})
 
 const UserAvatar = styled('img')({
   width: '50px',
   height: '50px',
   borderRadius: '50%',
   marginBottom: '8px',
-});
+})
 
 const UserName = styled(Typography)({
   textAlign: 'center',
   fontFamily:
-      'Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,Bitstream Vera Sans Mono, Courier New, monospace',
-});
+    'Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono,Bitstream Vera Sans Mono, Courier New, monospace',
+})
 
 interface Props {
-  items: ItemVoter[];
+  items: ItemVoter[]
 }
 
-const VotersList = ({items}: Props) => {
+const VotersList = ({ items }: Props) => {
   return (
     <Grid container spacing={2} justifyContent="center">
       {items.map((voter) => (
         <Grid key={voter.id} item xs={12} sm={6} md={4} lg={2}>
           <UserCard>
             <UserAvatar src={voter.profil_pic} alt={voter.name} />
-            <UserName color="text.secondary" variant="subtitle1">{voter.name}</UserName>
+            <UserName color="text.secondary" variant="subtitle1">
+              {voter.name}
+            </UserName>
           </UserCard>
         </Grid>
       ))}
     </Grid>
-  );
-};
+  )
+}
 
-export default function Voters({items}: Props) {
-  const listRef = useRef<HTMLDivElement>(null);
+export default function Voters({ items }: Props) {
+  const listRef = useRef<HTMLDivElement>(null)
 
   return (
     <Box sx={{ mt: 4 }} ref={listRef}>
-      <Typography variant="h5" align="center" color="text.secondary" gutterBottom>
+      <Typography
+        variant="h5"
+        align="center"
+        color="text.secondary"
+        gutterBottom
+      >
         Voters
       </Typography>
-      <VotersList items={items}/>
+      <VotersList items={items} />
     </Box>
-  );
+  )
 }
