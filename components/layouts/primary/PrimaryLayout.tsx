7256922b4cffe7eb8484b9785f6b821ff4dbfb4d
raja <raja.rakoto7@gmail.com>
2023-03-10 22:27:33 +0300

    set title

--- components/layouts/primary/PrimaryLayout.tsx
+++ components/layouts/primary/PrimaryLayout.tsx
@@ -9,7 +9,7 @@ const PrimaryLayout: React.FC<IPrimaryLayout> = ({ children }) => {
   return (
     <>
       <Head>
-        <title>Primary Layout Example</title>
+        <title>Web Ranking</title>
       </Head>
       <main className={styles.main}>{children}</main>
     </>
