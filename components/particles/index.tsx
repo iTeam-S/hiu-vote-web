0a6a8450e87163821801296a015e88218f8f6524
Raja Rakotonirina <raja.rakoto7@gmail.com>
2023-03-10 19:42:05 +0000

    Design

--- components/particles/index.tsx
+++ components/particles/index.tsx
@@ -40,6 +40,7 @@ export default function Particle() {
           },
         },
         particles: {
+          reduceDuplicates: false,
           color: {
             value: '#ffffff',
           },
