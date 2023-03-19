893ad32aa265c52cad1d3c906185102bfab8f005
raja <raja.rakoto7@gmail.com>
2023-03-19 16:11:26 +0300

    fix useEffect -> useLayoutEffect

--- components/layouts/sidebar/SidebarLayout.tsx
+++ components/layouts/sidebar/SidebarLayout.tsx
@@ -1,33 +1,34 @@
 import { AppBar, Stack, Toolbar, Typography } from '@mui/material'
 import Image from 'next/image'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useLayoutEffect } from 'react'
 import styles from './SidebarLayout.module.css'
 import { animateScroll as scroll } from 'react-scroll'
 import { FaUsers } from 'react-icons/fa'
 
-function scrollToComponent() {
-  const element = document.querySelector('#contrib-link') as HTMLElement
-  if (element !== null) {
-    scroll.scrollTo(element.offsetTop, {
-      duration: 1000,
-      delay: 100,
-      smooth: true,
-      offset: -70,
-    })
-  }
-}
-
 export interface ISidebarLayout {}
 
 const SidebarLayout: React.FC<ISidebarLayout> = () => {
-  const [widthScreen, setwidthScreen] = useState(window.innerWidth)
+  const [widthScreen, setWidthScreen] = useState<number>(0)
   const isMobileScreen = widthScreen <= 656
 
-  useEffect(() => {
-    const handleResize = () => setwidthScreen(window.innerWidth)
+  useLayoutEffect(() => {
+    const handleResize = () => setWidthScreen(window.innerWidth)
+    handleResize()
     window.addEventListener('resize', handleResize)
     return () => window.removeEventListener('resize', handleResize)
   }, [])
+
+  function scrollToComponent() {
+    const element = document.querySelector('#contrib-link') as HTMLElement
+    if (element !== null) {
+      scroll.scrollTo(element.offsetTop, {
+        duration: 1000,
+        delay: 100,
+        smooth: true,
+        offset: -70,
+      })
+    }
+  }
 
   return (
     <AppBar position="fixed" color="secondary" className={styles.navbar}>
