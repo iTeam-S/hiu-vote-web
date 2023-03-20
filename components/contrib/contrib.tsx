76682f2a68a7fbb1d0f54065ac3b1a2b7a61a07c
Gaetan Jonathan BAKARY <gaetan.s118@gmail.com>
2023-03-20 01:26:37 +0000

    Minor improv

--- components/contrib/contrib.tsx
+++ components/contrib/contrib.tsx
@@ -20,18 +20,18 @@ export default function Contrib() {
 
   const linkLinkedin: string[] = [
     'https://www.linkedin.com/in/ntsoa/',
-    'https://www.linkedin.com/in/gaetanj/',
-    'https://www.linkedin.com/mwlite/in/vololoniaina-damia-tsiafaratiana-b0394318a',
     'https://portfolio-raja.netlify.app/',
+    'https://www.linkedin.com/mwlite/in/vololoniaina-damia-tsiafaratiana-b0394318a',
+    'https://www.linkedin.com/in/gaetanj/',
     'https://www.linkedin.com/in/rajaonarivony/',
   ]
 
   useEffect(() => {
     const usernames: string[] = [
       'Ntsoa2112',
-      'Gaetan1903',
-      'Damichou',
       'RajaRakoto',
+      'Damichou',
+      'Gaetan1903',
       'rivo2302',
     ]
     const promises: Promise<IUser>[] = usernames.map((username) =>
@@ -58,7 +58,7 @@ export default function Contrib() {
         <TypingEffect
           user="root"
           host="hiu"
-          lists={["echo 'Contributeurs'"]}
+          lists={["echo $Contributeurs"]}
           root={true}
         />
       </div>
