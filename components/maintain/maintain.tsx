819cebda0060824bda2c537d7253f416d876c6b6
raja <raja.rakoto7@gmail.com>
2023-03-24 17:43:42 +0300

    add/apply react-under-construction

---
+++ components/maintain/maintain.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+/* libs */
+import UnderConstruction from 'react-under-construction'
+
+/* styles */
+import 'react-under-construction/build/css/index.css'
+
+// ===========================================================
+
+export default function Maintain() {
+  return (
+    <UnderConstruction
+      logo={{
+        src: 'https://iteam-s.mg/assets/img/LOGO.png',
+        alt: 'iteam-s logo',
+      }}
+      title={{
+        text: 'I-Team$',
+      }}
+      description={{
+        text: 'Notre site internet est en construction. Nous serons bientôt là, abonnez-vous pour être informé',
+        style: {
+          maxWidth: '440px',
+        },
+      }}
+      subscribe={{
+        placeholder: 'E-mail',
+        buttonText: "S'abonner",
+        onSubmit: (value: string) => {
+          console.log('user typed email :', value)
+        },
+      }}
+      links={
+        [
+          // {
+          //   url: 'https://web.facebook.com/iTeam.Community',
+          //   image: '/public/facebook.svg',
+          // },
+          // {
+          //   url: 'https://www.linkedin.com/',
+          //   image: '../../public/linkedin.svg',
+          // },
+        ]
+      }
+    />
+  )
+}
