b26e9c25ae37b4ef6c9c18bfb0281ccac29f15fc
raja <raja.rakoto7@gmail.com>
2023-03-25 14:59:48 +0300

    reverse sponsor mapping | add timer for delay

--- components/sponsor/sponsor.tsx
+++ components/sponsor/sponsor.tsx
@@ -26,13 +26,27 @@ SwiperCore.use([Autoplay])
 
 export default function Sponsor() {
   const [sponsors, setSponsors] = useState<I_SponsorsList | null>(null)
+  const [swiperAutoplayDelay, setSwiperAutoplayDelay] = useState(10000)
+
   const fetchSponsors = async () => {
     const sponsorsData = await getSponsors()
-    if (sponsorsData) setSponsors(sponsorsData)
+    if (sponsorsData) {
+      setSponsors(sponsorsData)
+    }
   }
 
   useEffect(() => {
     fetchSponsors()
+  }, [])
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSwiperAutoplayDelay(3000)
+    }, 7000)
+
+    return () => {
+      clearTimeout(timer)
+    }
   }, [])
 
   return (
@@ -54,7 +68,7 @@ export default function Sponsor() {
       >
         <Swiper
           autoplay={{
-            delay: 3000,
+            delay: swiperAutoplayDelay,
             disableOnInteraction: false,
           }}
           slidesPerView={1}
@@ -81,29 +95,32 @@ export default function Sponsor() {
           }}
         >
           {sponsors &&
-            sponsors.items.map((sponsor) => (
-              <SwiperSlide
-                key={sponsor.id}
-                className="d-flex justify-content-center align-content-center align-items-center"
-              >
-                <div className={styles.mySwiper}>
-                  <img
-                    src={
-                      process.env.API_URL +
-                      'files/' +
-                      sponsor.collectionId +
-                      '/' +
-                      sponsor.id +
-                      '/' +
-                      sponsor.logo
-                    }
-                    alt={sponsor.name + ' logo'}
-                    width={200}
-                  />
-                  <h6>{sponsor.name}</h6>
-                </div>
-              </SwiperSlide>
-            ))}
+            sponsors.items
+              .slice()
+              .reverse()
+              .map((sponsor) => (
+                <SwiperSlide
+                  key={sponsor.id}
+                  className="d-flex justify-content-center align-content-center align-items-center"
+                >
+                  <div className={styles.mySwiper}>
+                    <img
+                      src={
+                        process.env.API_URL +
+                        'files/' +
+                        sponsor.collectionId +
+                        '/' +
+                        sponsor.id +
+                        '/' +
+                        sponsor.logo
+                      }
+                      alt={sponsor.name + ' logo'}
+                      width={200}
+                    />
+                    <h6>{sponsor.name}</h6>
+                  </div>
+                </SwiperSlide>
+              ))}
         </Swiper>
       </div>
     </React.Fragment>
